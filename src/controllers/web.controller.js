432bb4bbd6b86195c2ac0df217a41928d2193b67
ducanh2004 <ducanh2004@users.noreply.github.com>
2023-08-30 15:36:34 +0700

    nopbai

---
+++ src/controllers/web.controller.js
@@ -0,0 +1,41 @@
+const Product = require("./../models/product.model");
+
+exports.home = async(req,res)=>{
+    try {
+        const products = await Product.find();
+        res.render("home",{
+            products:products
+        });
+    } catch (error) {
+        
+    }
+
+}
+exports.formCreate = (req,res)=>{
+    const data = req.body;
+    data.url = req._parsedOriginalUrl.path;
+    res.render("form_create",{
+        product:data
+    });
+}
+
+exports.create = async (req,res)=>{
+    const data = req.body;
+    try {
+        const p = new Product(data);
+        await p.save();
+        res.redirect("/");
+    } catch (error) {
+        res.render("/form_create",{product:data,error:error});
+    }
+}
+
+exports.delete =  async (req,res)=>{
+    const _id = req.params.id;
+    try {
+        await Product.findByIdAndDelete(_id);
+        res.redirect("/");
+    } catch (error) {
+        res.redirect("/");
+    }
+}
