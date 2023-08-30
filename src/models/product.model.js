5da4c898536f0b4cf42b5e606f5f72d25eddc3ff
ducanh2004 <ducanh2004@users.noreply.github.com>
2023-08-30 15:37:37 +0700

    nopbai

---
+++ src/models/product.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const product_schema = new mongoose.Schema({
+    code : String,
+    name : String,
+    origin : Date,
+    price : Number,
+    qty : Number,
+    store : String,
+});
+module.exports = mongoose.model("Product",product_schema);
---
+++ src/routes/web.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("./../controllers/web.controller");
+
+router.get("/",controller.home);
+
+router.get("/create",controller.formCreate);
+router.post("/create",controller.create);
+
+router.get("/delete/:id",controller.delete);
+
+
+
+module.exports = router;
